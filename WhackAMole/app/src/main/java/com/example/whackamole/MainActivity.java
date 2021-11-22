b4e63691404b23b455c13151db5b7054b68506c5
Ilai <ilai.gigi@gmail.com>
2021-11-22 16:05:22 +0200

    Initialized new project

---
+++ WhackAMole/app/src/main/java/com/example/whackamole/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.whackamole;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity g
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
