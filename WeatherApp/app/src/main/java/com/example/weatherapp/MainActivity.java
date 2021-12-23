730745e4ab044128dce520011e48c2de607a8c9a
Ilai <ilai.gigi@gmail.com>
2021-12-23 16:13:18 +0200

    added xml

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    RadioButton rbSunny, rbPartlyCloudy, rbCloudy, rbRaining, rbSnowing, rbThunder;
+    RadioButton[] buttons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rbSunny = findViewById(R.id.rbSunny);
+        rbPartlyCloudy = findViewById(R.id.rbPartlyCloudy);
+        rbCloudy = findViewById(R.id.rbCloudy);
+        rbRaining = findViewById(R.id.rbRaining);
+        rbSnowing = findViewById(R.id.rbSnowing);
+        rbThunder = findViewById(R.id.rbThunder);
+        buttons = new RadioButton[6];
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
