ddc4a31ad9c85da4d3eed4ff9237c2a4f6d2bb9c
Ilai <ilai.gigi@gmail.com>
2022-01-17 17:08:37 +0200

    Finished Project

--- WorkerManagerExample/app/src/main/java/com/example/workermanagerexample/MainActivity.java
+++ WorkerManagerExample/app/src/main/java/com/example/workermanagerexample/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.workermanagerexample;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TimePicker picker;
+    Button btPushWork;
+    CheckBox cbRepetitive;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        picker = findViewById(R.id.timePicker1);
+        btPushWork = findViewById(R.id.btPushWork);
+        cbRepetitive = findViewById(R.id.cbRepetitive);
+
+        btPushWork.setOnClickListener(this);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public int calcDelay(int hour, int minute){
+        Date date = Calendar.getInstance().getTime();
+        int currentMinute = date.getMinutes();
+        int currentHour = date.getHours();
+        int minDiff = minute > currentMinute ? minute - currentMinute : currentHour - minute;
+        return Math.abs(currentHour - hour) * 60 + minDiff;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public void onClick(View view) {
+        if (view == btPushWork){
+            int scheduleHour = picker.getCurrentHour();
+            int scheduleMinute = picker.getCurrentMinute();
+            int delay = calcDelay(scheduleHour, scheduleMinute);
+            if (!cbRepetitive.isChecked())
+                initializeOneTimeWork(delay);
+            else
+                initializeRepetitiveWork(delay);
+        }
+    }
+
+    public void initializeOneTimeWork(int delay){
+        WorkRequest myWorkRequest =
+                new OneTimeWorkRequest.Builder(MyWorker.class)
+                        .setInitialDelay(delay, TimeUnit.MINUTES)
+                        .build();
+        WorkManager
+                .getInstance(this)
+                .enqueue(myWorkRequest);
+    }
+
+    public void initializeRepetitiveWork(int delay){
+        WorkRequest myWorkRequest =
+                new PeriodicWorkRequest.Builder(MyWorker.class, 3, TimeUnit.MINUTES)
+                        .setInitialDelay(delay, TimeUnit.MINUTES)
+                        .build();
+        WorkManager
+                .getInstance(this)
+                .enqueue(myWorkRequest);
     }
 }
--- WorkerManagerExample/app/src/main/java/com/example/workermanagerexample/MyWorker.java
+++ WorkerManagerExample/app/src/main/java/com/example/workermanagerexample/MyWorker.java
@@ -1,4 +1,59 @@
 package com.example.workermanagerexample;
 
-public class MyWorker {
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.WorkerThread;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class MyWorker extends Worker {
+    public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.damndaniel);
+        mediaPlayer.start();
+
+        createNotificationChannel();
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "CHANNEL_ID")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Alarms")
+                .setContentText("The time has come")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(false);
+
+        builder.setContentText("Successes");
+        notificationManager.notify(10, builder.build());
+        return Result.success();
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            CharSequence name = getApplicationContext().getString(R.string.channel_name);
+            String description = getApplicationContext().getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getApplicationContext().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 }
