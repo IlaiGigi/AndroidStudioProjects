90f0684a0dc72bd370cce69682306b3f121e93fa
Ilai <ilai.gigi@gmail.com>
2022-02-07 16:40:57 +0200

    Initialized Project.

--- GPSExample/app/src/main/java/com/example/gpsexample/MainActivity.java
+++ GPSExample/app/src/main/java/com/example/gpsexample/MainActivity.java
@@ -1,14 +1,104 @@
 package com.example.gpsexample;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
+
+    LocationManager locationManager;
+    LocationProvider locationProvider;
+    TextView tvData;
+    Button btGetLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        requestPermission(Manifest.permission.INTERNET);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+
+        tvData = findViewById(R.id.tvData);
+        btGetLocation = findViewById(R.id.btGetLocation);
+
+        btGetLocation.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double locLat = location.getLatitude();
+        double locLon = location.getLongitude();
+        tvData.setText(String.format("Longitude: %f\nLatitude: %f", locLon, locLat));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onClick(View view) {
+        if (view == btGetLocation) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+        }
+    }
+
+    public boolean permissionGranted(String permission)
+    {
+        // Check if the app is api 23 or above
+        // If so, must ask for permission.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if (checkSelfPermission(permission) ==
+                    PackageManager.PERMISSION_GRANTED)
+            {
+                Log.d("Permissions", "permission granted");
+                return true;
+            }
+            Log.d("Permissions", "Permission was not granted");
+            return false;
+        }
+        // Permission is automatically granted on sdk<23 upon installation
+        Log.d("Permissions", "permission granted");
+        return true;
+    }
+
+    public boolean requestPermission(String permission)
+    {
+        if (permissionGranted(permission))
+            return true;
+        // Ask for permission
+        ActivityCompat.requestPermissions(this, new String[]{permission}, 1);
+        return false;
     }
 }
