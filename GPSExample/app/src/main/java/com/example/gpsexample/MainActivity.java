0063e331aeefa1a0579b4f07264a77ab69afb48b
Ilai <ilai.gigi@gmail.com>
2022-02-24 13:47:39 +0200

    Initialized project

--- GPSExample/app/src/main/java/com/example/gpsexample/MainActivity.java
+++ GPSExample/app/src/main/java/com/example/gpsexample/MainActivity.java
@@ -6,6 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -22,35 +30,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener, OnMapReadyCallback {
 
     LocationManager locationManager;
     LocationProvider locationProvider;
+    GoogleMap myMap;
     TextView tvData;
     Button btGetLocation;
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermission(Manifest.permission.ACCESS_COARSE_LOCATION);
         requestPermission(Manifest.permission.ACCESS_FINE_LOCATION);
         requestPermission(Manifest.permission.INTERNET);
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         locationProvider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
 
+        mapView = findViewById(R.id.map);
         tvData = findViewById(R.id.tvData);
         btGetLocation = findViewById(R.id.btGetLocation);
 
         btGetLocation.setOnClickListener(this);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
     }
 
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
-        double locLat = location.getLatitude();
-        double locLon = location.getLongitude();
-        tvData.setText(String.format("Longitude: %f\nLatitude: %f", locLon, locLat));
+        double lat = location.getLatitude();
+        double lng = location.getLongitude();
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(new LatLng(lat, lng));
+        markerOptions.title("MyMarker");
+        myMap.addMarker(markerOptions);
+        tvData.setText(String.format("Longitude: %f\nLatitude: %f", lng, lat));
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
@@ -100,5 +119,84 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         // Ask for permission
         ActivityCompat.requestPermissions(this, new String[]{permission}, 1);
         return false;
+    }
+
+    public void requestLocationPermission() {
+        // Check if the app is api 23 or above
+        // If so, must ask for permission.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            String coarsePermission = Manifest.permission.ACCESS_COARSE_LOCATION;
+            String finePermission = Manifest.permission.ACCESS_FINE_LOCATION;
+            String[] permissions = new String[]{coarsePermission, finePermission};
+            requestPermissions(permissions, 1);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[]
+            permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        String coarsePermission = Manifest.permission.ACCESS_COARSE_LOCATION;
+        String finePermission = Manifest.permission.ACCESS_FINE_LOCATION;
+        if (grantResults.length > 0 &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            if (checkSelfPermission(finePermission) !=
+                    PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(coarsePermission) !=
+                            PackageManager.PERMISSION_GRANTED)
+            {
+                Log.d("Permissions", "Permissions was not granted");
+                return;
+            }
+            Log.d("Permissions", "Permissions was granted");
+            // Register to get location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    10000, // 10-second interval.
+                    10, // 10 meters.
+                    this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        myMap = googleMap;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
